cf86ad9ba9244bc224a77feb9df157733e1dfd42
Jalen Lyu <jalen.lyu@gmail.com>
2014-04-18 00:48:46 +0800

    restart

--- build/build.sh
+++ build/build.sh
@@ -2,23 +2,34 @@
 # Learn more at http://developer.longtailvideo.com/trac/wiki/PluginsCompiling
 # To use, make sure you have downloaded and installed the Flex SDK in the following directory:
 
-FLEXPATH=/home/lusurf/Apps/flex_sdk_4.6
+FLEXPATH=/opt/flex-4.6
 
-echo ""
-echo "Compiling helloOSMF.swf"
-echo ""
-
-$FLEXPATH/bin/mxmlc ../src/com/lusurf/HelloOSMF.as -sp ../src -o ../helloOSMF.swf -use-network=false -optimize=true -incremental=false -target-player="11.1" -static-link-runtime-shared-libraries=true -default-size 480 270 -default-background-color=0x000000
 echo ""
 echo "Compiling chromelessPlayer.swf"
 echo ""
 
-$FLEXPATH/bin/mxmlc ../src/ChromelessPlayer.as -sp ../src -o ../chromelessPlayer.swf -use-network=false -optimize=true -incremental=false -target-player="11.1" -static-link-runtime-shared-libraries=true -default-size 480 270 -default-background-color=0x000000
+$FLEXPATH/bin/mxmlc ../src/ChromelessPlayer.as \
+	-sp ../src \
+	-o ../chromelessPlayer.swf \
+	-use-network=false \
+	-optimize=true \
+	-incremental=false \
+	-target-player="11.1" \
+	-static-link-runtime-shared-libraries=true \
+	-default-size 480 270 \
+	-default-background-color=0x000000
 
 echo ""
 echo "Compiling adaptiveProvider.swf"
 echo ""
 
-$FLEXPATH/bin/mxmlc ../src/com/longtailvideo/jwplayer/media/AdaptiveProvider.as -sp ../src -o ../adaptiveProvider.swf -library-path+=../libs -load-externs=../libs/jwplayer-5-classes.xml  -use-network=false -optimize=true -incremental=false -target-player="11.1" -static-link-runtime-shared-libraries=true
-
-#cp ../adaptiveProvider.swf ~/Workspace/hello/assets/adaptiveProvider_debug.swf
+$FLEXPATH/bin/mxmlc ../src/com/longtailvideo/jwplayer/media/AdaptiveProvider.as \
+	-sp ../src \
+	-o ../adaptiveProvider.swf \
+	-library-path+=../libs \
+	-load-externs=../libs/jwplayer-5-classes.xml \
+	-use-network=false \
+	-optimize=true \
+	-incremental=false \
+	-target-player="11.1" \
+	-static-link-runtime-shared-libraries=true
--- example/streams.js
+++ example/streams.js
@@ -1,17 +1,7 @@
 var teststreams = [
     {
-        file:'http://hello.htlv.dev.aifang.com/ia/list.m3u8',
-        title: 'Ice Age',
-        description: '冰河世纪'
+        file:'http://127.0.0.1:1234/csyx.m3u8',
+        title: '冲上云霄',
+        description: '冲上云霄II主题曲'
     },
-    {
-        file:'http://hello.htlv.dev.aifang.com/cyzn/list.m3u8',
-        title: '彩云之南',
-        description: '年会节目，彩云之南，索尼SK17i拍摄，Android系统'
-    },
-    {
-        file:'http://hello.htlv.dev.aifang.com/welcome/list.m3u8',
-        title: '欢迎加入安居客',
-        description: '欢迎加入安居客'
-    }
 ];
